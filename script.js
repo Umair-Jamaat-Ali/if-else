72f799233e8f0a117dadb5d1a2ffd94f06290920
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-03-26 00:15:52 +0500

    if else

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+// write a program to check the lucky draw...
+var y = prompt("Entre your three digits code");
+
+
+
+if(y==="10927"){
+console.log("Congratulation");
+}
+else{
+    console.log("Best luck for next time");
+}
+
+// write a program that prompt to check the students grade , which students entre the score and progream tell us grades..
+
+
+var studentScore = prompt("Entre your Marks");
+
+if(studentScore<50){
+    console.log("You are failed!")
+}
+
+
+else if(studentScore<60){
+    console.log("Your grades is D")
+}
+
+
+else if(studentScore<70){
+    console.log("Your grades is C")
+}
+
+
+
+else if(studentScore<80){
+    console.log("Your grades is B")
+}
+
+
+else if(studentScore<90){
+    console.log("Your grades is A")
+}
+
+
+else if(studentScore<100){
+    console.log("Your grades is A+")
+}
+
+
+// write a program which check that, the number is odd or even?
+
+var x = prompt("Enter a Number");
+
+if(x%2==0){
+    console.log("number is even");
+}
+else{
+    console.log("Number is odd")
+}
